cc7aae5397968b1e4abcfbe67dc63f9148307767
ajb-4 <alexbrown4711@gmail.com>
2023-08-22 16:55:24 -0400

    done with first attempt, need to edit/debug everything

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,4 +1,12 @@
-Add your import statements for View and Game here
+// Add your import statements for View and Game here
+
+import View from "/Users/alexbrown/Documents/app-academy/classwork/W9D2/practice-for-ch-js-browser-ttt-main/src/ttt-view.js";
+
+import Game from "/Users/alexbrown/Documents/app-academy/classwork/W9D2/practice-for-ch-js-browser-ttt-main/ttt_node/game.js";
+
+const game = new Game();
+
+const view = document.getElementById("ttt");
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,17 +1,48 @@
 class View {
   constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard();
+    //
+    this.el.innerHTML += document.getElementById('line 8 ul element');
   }
   
   setupBoard() {
+    const board = document.createElement('ul')
+    for (let i = 1; i < 4; i++) {
+      for (let j = 1; j < 4; j++) {
+        let coords = [];
+        coords.push(i);
+        coords.push(j);
+        const square = document.createElement('li')
+        //
+        // <li data-index-number=`${coords}` id="square"></li>
+        board.appendChild(square);
+      }
+    }
+    return board
+    //install handleClick(e)
   }
   
   handleClick(e) {
+    const position = querySelector("li:hover");
+    let ele = e.target;
+    //
+    const pos = position.data-index-number;
+    Game.playMove(pos);
+    ele.classList.add("e.team");
+
   }
 
   makeMove(square) {
+    
+   this.handleGameOver()
   }
   
   handleGameOver() {
+    if (Game.winner) {
+      return `Congrats ${Game.winner}, you have won!!!`
+    }
   }
 }
 
